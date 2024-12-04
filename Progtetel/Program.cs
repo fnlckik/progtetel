ed733aae9e2d75bad3bc3d70de166e7c422a4ea9
fnlckik <farkas.norbert@ckik.hu>
2024-12-04 11:16:55 +0100

    párosak kiválogatása

--- Progtetel/Program.cs
+++ Progtetel/Program.cs
@@ -9,6 +9,22 @@ namespace Progtetel
             int[] x = { 7, 2, -4, 5, 12, 1 };
 
             //Párosok kiválogatása - 1
+            int[] parosak = new int[100];
+            int db = 0;
+            int i;
+            for (i = 0; i < x.Length; i++)
+            {
+                if (x[i] % 2 == 0)
+                {
+                    parosak[db] = x[i];
+                    db++;
+                }
+            }
+            for (i = 0; i < db; i++)
+            {
+                Console.Write(parosak[i] + " ");
+            }
+            Console.WriteLine();
 
             //Maximum kiválasztás - 2
         }
